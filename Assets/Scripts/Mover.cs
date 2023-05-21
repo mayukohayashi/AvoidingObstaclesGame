053ae16742c477f83562a3522e38980a7858c4b3
mayukohayashi <mayuko.hayashi.spf.rd@gmail.com>
2023-05-21 18:04:49 +0900

    add: instractions and cleanup

--- Assets/Scripts/Mover.cs
+++ Assets/Scripts/Mover.cs
@@ -9,14 +9,27 @@ public class Mover : MonoBehaviour
   // Start is called before the first frame update
   void Start()
   {
+    PrintInstructions();
   }
 
   // Update is called once per frame
   void Update()
   {
-    float x = Input.GetAxis("Horizontal") * Time.deltaTime * moveSpeed;
-    float z = Input.GetAxis("Vertical") * Time.deltaTime  * moveSpeed;
+    MovePlayer();
+  }
 
+  void PrintInstructions()
+  {
+    Debug.Log("Welcome to the sparrow game");
+    Debug.Log("Move your player with WASD or arrow keys");
+    Debug.Log("Escape from the obstacles. Don't hit the walls!");
+  }
+
+  void MovePlayer()
+  {
+    float x = Input.GetAxis("Horizontal") * Time.deltaTime * moveSpeed;
+    float z = Input.GetAxis("Vertical") * Time.deltaTime * moveSpeed;
     transform.Translate(x, 0, z);
   }
+
 }
